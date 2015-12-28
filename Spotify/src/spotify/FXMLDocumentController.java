91db61ba47c9740200ba1925d4ce30f038cd78fe
Andrea <Andrea@172.21.112.109>
2015-12-28 17:38:05 +0100

    Update graphics
    - added a left side bar with 2 menus represented by ListView(YOUR MUSIC
    and PLAYLIST)

--- Spotify/src/spotify/FXMLDocumentController.java
+++ Spotify/src/spotify/FXMLDocumentController.java
@@ -1,31 +1,87 @@
-
 package spotify;
 
+import graphics.ListItem;
+import graphics.ListItemRenderer;
+import graphics.MusicMenu;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
 
 /**
+ * FXML Controller class of "FXMLDocument.fxml"
+ * 
  * @author Antonioni Andrea & Zanelli Gabriele
  */
 
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private Label username;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private ImageView previousButton;
+    @FXML
+    private ImageView playButton;
+    @FXML
+    private ImageView nextButton;
+    @FXML
+    private Label countUP;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Label countDown;
+    @FXML
+    private ImageView shuffle;
+    @FXML
+    private ImageView replay;
+    @FXML
+    private Slider volumeControl;
+    @FXML
+    private HBox newPlaylistButton;
+    @FXML
+    private ImageView artwork;
+    @FXML
+    private Label artist;
+    @FXML
+    private Label titleSong;
+    @FXML
+    private ListView<ListItem> musicMenu, playlistsMenu;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        initLeftSideBar();
+        
     }    
     
+    private void initLeftSideBar()
+    {
+        musicMenu.setItems(MusicMenu.createMusicMenu());
+        
+        //Says to musicMenu how to render elements
+        musicMenu.setCellFactory(new Callback<ListView<ListItem>, ListCell<ListItem>>() {
+
+            @Override
+            public ListCell<ListItem> call(ListView<ListItem> param) {
+                return new ListItemRenderer();
+            }
+        });
+        
+        //Says to playlistMenu how to render elements
+        playlistsMenu.setCellFactory(new Callback<ListView<ListItem>, ListCell<ListItem>>() {
+
+            @Override
+            public ListCell<ListItem> call(ListView<ListItem> param) {
+                return new ListItemRenderer();
+            }
+        });
+    }
 }
