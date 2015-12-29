81f026a561974338d0dd197463f2f6d69c9fedbd
Andrea Antonioni <andreaantonioni97@gmail.com>
2015-12-29 09:50:36 +0100

    Update graphics left side bar

--- Spotify/src/graphics/ListItemRenderer.java
+++ Spotify/src/graphics/ListItemRenderer.java
@@ -4,8 +4,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 /**
  * Contains the way to represents graphically ListItem objects inside the ListView.
@@ -20,27 +22,27 @@ public class ListItemRenderer extends ListCell<ListItem>{
     protected void updateItem(ListItem item, boolean empty) {
         super.updateItem(item, empty);
         setGraphic(createPane());
-
     }
-
+    
     private Pane createPane() {
         ListItem item = getItem();
         if(item == null)
             return null;
         
-        BorderPane borderPane = new BorderPane();
-
-        Label description = new Label(item.getDescription());
-        description.setFont(new Font("Helvetica", 14));
-        borderPane.setCenter(description);
+        HBox hboxPane= new HBox();
         
         if(item.getUrlIcon() != null && !item.getUrlIcon().equals(""))
         {
             ImageView icon = new ImageView(item.getUrlIcon());
-            borderPane.setLeft(icon);
+            hboxPane.getChildren().add(icon);
         }
+
+        Label description = new Label(item.getDescription());
+        description.setFont(new Font("Helvetica", 14));
+        description.setTextAlignment(TextAlignment.LEFT);
+        hboxPane.getChildren().add(description);
         
-        return borderPane;
+        return hboxPane;
     }
 
 }
